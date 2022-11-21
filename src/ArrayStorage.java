781ad649e89342a835e5ab9c965d10d1cdfc8359
Dima <planeta977@mail.ru>
2022-11-21 08:53:58 +0300

    черновик

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -2,16 +2,34 @@
  * Array based storage for Resumes
  */
 public class ArrayStorage {
+
+    private int cntResume = 0;
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        for (int i = 0; i < cntResume; i++) {
+            storage[i] = null;
+        }
+        cntResume = 0;
     }
 
     void save(Resume r) {
+        if (r.uuid != null) {
+            storage[cntResume] = r;
+            cntResume++;
+        }
     }
 
     Resume get(String uuid) {
-        return null;
+        Resume res = null;
+
+        for (Resume r : storage) {
+            if (r.uuid.equals(uuid)) {
+                res = r;
+                break;
+            }
+        }
+        return res;
     }
 
     void delete(String uuid) {
@@ -21,10 +39,15 @@ public class ArrayStorage {
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] res = new Resume[cntResume];
+
+        for (int i = 0; i < cntResume; i++) {
+            res[i] = storage[i];
+        }
+        return res;
     }
 
     int size() {
-        return 0;
+        return cntResume;
     }
 }
